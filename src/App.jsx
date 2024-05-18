b72d11b802a885215346d2905303a73925ec2f0d
Sahil <sahilsharma86600@gmail.com>
2024-05-18 17:45:41 +0530

    card add

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { Card } from "./Components/Card";
 
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold ">journalSquads</h1>
+      <Card />
     </>
   );
 }
