a7b8e778f8bcfd098a52aa52760d84b9ba8e468f
Kartik <kartikagarwal20144@gmail.com>
2024-06-19 19:30:34 +0530

    try4

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   // build: {
+//   //   outDir: 'dist', // Specify the publish directory here
+//   //   assetsDir: '', // Optional: Directory for assets within outDir
+//   //   // Other build options
+//   // },
+// })
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-  // build: {
-  //   outDir: 'dist', // Specify the publish directory here
-  //   assetsDir: '', // Optional: Directory for assets within outDir
-  //   // Other build options
-  // },
-})
+  build: {
+    outDir: 'dist',
+    assetsDir: '',
+  },
+});
