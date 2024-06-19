2f50a0ce25899248bb1a7ce0279dd87ef3a708fa
Kartik <kartikagarwal20144@gmail.com>
2024-06-19 19:14:53 +0530

    try3

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Specify the publish directory here
-    assetsDir: '', // Optional: Directory for assets within outDir
-    // Other build options
-  },
+  // build: {
+  //   outDir: 'dist', // Specify the publish directory here
+  //   assetsDir: '', // Optional: Directory for assets within outDir
+  //   // Other build options
+  // },
 })
