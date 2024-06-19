fed5333e72988e17595e975cb161bc7197949781
Kartik <kartikagarwal20144@gmail.com>
2024-06-19 19:07:37 +0530

    try to fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Custm from "./components/Home/Custm";
 import Hero from "./components/Home/Hero";
 import Journal from "./components/Journal/Journal";
